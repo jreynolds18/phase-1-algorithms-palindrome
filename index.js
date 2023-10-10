443bc5d86e46f1dbdf72ea983ff1a332348e682f
Jesse Reynolds <jessereynolds18@gmail.com>
2023-10-09 21:35:45 -0500

    make a function detect if an input word is a palindrome

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let backwardsWord = word.split('').reverse().join('');
+  if (word === backwardsWord) {
+    return true;
+  } else if (word !== backwardsWord) {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+  I need a function that returns true or false based on an input
+    Take every letter, split it apart, put it in an array
+    Rejoin the letters into a word and no longer an array
+    If that word is the same as before it was split apart, returns true
+    If that word is not the same as before it was split, returns false
 */
 
 /*
-  Add written explanation of your solution here
+  First, I created a backwardsWord variable, where I used methods to split the input word into letters in an array, reversed the array, and rejoined it into a string.
+  If the word is the same as the backwardsWord, it is true and therefore a palindrome
+  If the word is not the same as the backwardsWord, it is false and not a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
